812956f1a1a03d6ea489b8bf420aabfb78636e8f
Fabio <nanook@nanook.com.br>
2016-03-11 15:59:43 -0300

    tracebacker

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ try:
     import simplejson as json
 except:
     import json
+import traceback
 import re
 from flask import Flask, request, render_template
 from json_schema.json_differ import diff_color_string, diff
@@ -32,18 +33,26 @@ def formater(result):
 @app.route("/", methods=("GET", "POST"))
 def home():
     """Pagina inicial."""
-    json_a = request.form.get("json_a", "")
-    json_b = request.form.get("json_b", "")
-    r = []
-    equal = False
-    if json_a and json_b:
-        equal = json_a == json_b
-        r = formater(diff_color_string(json_a, json_b))
-    context = {"json_a": json_a,
-               "json_b": json_b,
-               "result": r,
-               "equal": equal}
-    return render_template("home.html", **context)
+    try:
+        if request.method == "GET":
+            return render_template("home.html", equal=None)
+
+        json_a = request.form.get("json_a", "")
+        json_b = request.form.get("json_b", "")
+        r = []
+        equal = False
+        if json_a and json_b:
+            equal = json_a == json_b
+            r = formater(diff_color_string(json_a, json_b))
+        context = {"json_a": json_a,
+                   "json_b": json_b,
+                   "result": r,
+                   "equal": equal}
+        return render_template("home.html", **context)
+    except Exception, e:
+        error = traceback.format_exc()
+        return render_template("home.html", error=error)
+
 
 if __name__ == "__main__":
     app.use_reloader = USE_RELOADER
